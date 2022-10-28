7d8031337d090008d8a8664b2460e3666dd99f54
2bsyed <114306673+2bsyed@users.noreply.github.com>
2022-10-28 21:47:24 +0600

    d1

---
+++ C/Beecrowd Beginner/2756.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+int main(){
+    printf("       A\n      B B\n     C   C\n    D     D\n   E       E\n    D     D\n     C   C\n      B B\n       A\n");
+       return 0; 
+}
--- C/Beecrowd Beginner/2763.c
+++ C/Beecrowd Beginner/2763.c
@@ -2,5 +2,5 @@
 int main(){
     int a,b,c,d;
     scanf("%d.%d.%d-%d",&a,&b,&c,&d);
-    printf("%d\n%d\n%d\n%d\n",a,b,c,d);
+    printf("%03d\n%03d\n%03d\n%02d\n",a,b,c,d);
 }
