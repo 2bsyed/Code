ac1204b8242916b27b845ceb5a3afd5c9f32a749
2bsyed <114306673+2bsyed@users.noreply.github.com>
2025-02-10 17:29:34 +0600

    d1

--- C++/hello.cpp
+++ C++/hello.cpp
@@ -1,5 +1,15 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
+
 int main(){
-    cout<<abs(-10);
+    vector<char> v(5);
+    for (auto &i : v){
+        cin >> i;
+    }
+    for (const auto &i : v){
+        cout << i << " ";
+    }
+    cout << endl; // Add a newline for better output formatting
 }
